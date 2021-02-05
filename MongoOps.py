7e54ed913038f45a85337d7ccf0353562ba391b1
Ricardo Montesanti <ricardomsanti@gmail.com>
2021-02-05 19:25:29 -0300

    Separating classes into different functionallities. Also, adde a mind map explaiging the whole program flow

--- MongoOps.py
+++ MongoOps.py
@@ -1,16 +1,6 @@
 from pymongo import MongoClient
-
-
-          
-            
-    
+  
         
-"""mo = MongoOps(client=MongoClient("localhost", 27017))
-db_name = mo.new_newdb()
-field_dict = mo.db_newfields()
-doc_list = mo.db_newcol(field_dict)
-createcol = mo.db_createcol(doc_list=doc_list, db_name=db_name)
-"""
 client = MongoClient("localhost", 27017)
 db_names = client.database_names()
 db = client.get_database("db_name")
@@ -21,4 +11,4 @@ col.count()
 col.insert()
 col.update()
 col.remove()
-col.
+
